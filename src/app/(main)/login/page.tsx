583e3e9093f73156ae0fde0c43c90828f27be4ac
rikiswin <103475494@student.swin.edu.au>
2024-12-11 02:55:37 +1100

    Refactored train page to be server component

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function page() {
   return (
     <div className="flex min-h-screen">
-      <div className="m-auto h-52 w-52 flex items-center justify-center border-2">
+      <div className="m-auto h-52 w-52 flex items-center justify-center rounded-xl shadow border-2">
         <GoogleSignInButton />
       </div>
     </div>
--- src/app/(main)/train/page.tsx
+++ src/app/(main)/train/page.tsx
@@ -1,28 +1,16 @@
-"use client";
-
-import { FilePond } from "react-filepond";
-import "filepond/dist/filepond.min.css";
-import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import FileUploadSection from "@/components/FileUploadSection";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
-export default function Home() {
-  const session = useSession();
-
-  if (session.status !== "authenticated") {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
     redirect("/login");
   }
   return (
     <main>
-      <div className="items-center justify-between p-24">
-        <FilePond
-          labelIdle='<b>*ONLY PDF Files*</b> Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
-          allowMultiple={false}
-          credits={false}
-          server={{
-            url: "/api/upload",
-          }}
-        />
-      </div>
+      <FileUploadSection />
     </main>
   );
 }
